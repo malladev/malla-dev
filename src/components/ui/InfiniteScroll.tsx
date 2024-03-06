8dc18754a9bd2b89126b271363719406b848ecdc
malla223 <konelamine1122@gmail.com>
2024-03-06 15:12:04 +0000

    create tech infinite scroll

---
+++ src/components/ui/InfiniteScroll.tsx
@@ -0,0 +1,112 @@
+import { cn } from "../../utils/cn.ts";
+import React, { useEffect, useState } from "react";
+
+export const InfiniteScroll = ({
+    items,
+    direction = "left",
+    speed = "fast",
+    pauseOnHover = true,
+    className,
+}: {
+    items: {
+        picture: string;
+        name: string;
+    }[];
+    direction?: "left" | "right";
+    speed?: "fast" | "normal" | "slow";
+    pauseOnHover?: boolean;
+    className?: string;
+}) => {
+    const containerRef = React.useRef<HTMLDivElement>(null);
+    const scrollerRef = React.useRef<HTMLUListElement>(null);
+
+    useEffect(() => {
+        addAnimation();
+    }, []);
+    const [start, setStart] = useState(false);
+    function addAnimation() {
+        if (containerRef.current && scrollerRef.current) {
+            const scrollerContent = Array.from(scrollerRef.current.children);
+
+            scrollerContent.forEach((item) => {
+                const duplicatedItem = item.cloneNode(true);
+                if (scrollerRef.current) {
+                    scrollerRef.current.appendChild(duplicatedItem);
+                }
+            });
+
+            getDirection();
+            getSpeed();
+            setStart(true);
+        }
+    }
+    const getDirection = () => {
+        if (containerRef.current) {
+            if (direction === "left") {
+                containerRef.current.style.setProperty(
+                    "--animation-direction",
+                    "forwards"
+                );
+            } else {
+                containerRef.current.style.setProperty(
+                    "--animation-direction",
+                    "reverse"
+                );
+            }
+        }
+    };
+    const getSpeed = () => {
+        if (containerRef.current) {
+            if (speed === "fast") {
+                containerRef.current.style.setProperty("--animation-duration", "30s");
+            } else if (speed === "normal") {
+                containerRef.current.style.setProperty("--animation-duration", "40s");
+            } else {
+                containerRef.current.style.setProperty("--animation-duration", "50s");
+            }
+        }
+    };
+    return (
+        <div
+            ref={containerRef}
+            className={cn(
+                "scroller relative z-20  max-w-7xl overflow-hidden  [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
+                className
+            )}
+        >
+            <ul
+                ref={scrollerRef}
+                className={cn(
+                    " flex min-w-full shrink-0 gap-4 py-4 w-max flex-nowrap",
+                    start && "animate-scroll ",
+                    pauseOnHover && "hover:[animation-play-state:paused]"
+                )}
+            >
+                {items.map((item, idx) => (
+                    <li
+                        className="max-w-full relative rounded-lg border border-b-0 flex-shrink-0 border-slate-700 px-8 py-2"
+                        style={{
+                            background:
+                                "linear-gradient(180deg, var(--slate-800), var(--slate-900)",
+                        }}
+                        key={item.name}
+                    >
+                        <div>
+                            <div
+                                aria-hidden="true"
+                                className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5"
+                            ></div>
+                            <div className="flex items-center justify-center gap-2">
+
+                                <img src={item.picture} alt={item.name} className="w-8" />
+                                <span className=" relative z-20 text-lg font-semibold leading-[1.6] text-slate-700">
+                                    {item.name}
+                                </span>
+                            </div>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
